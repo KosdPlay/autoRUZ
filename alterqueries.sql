3cb2c3c89996bbedf601ff99f1a530897594a0da
rinatsaf <rinatsafiullin290606@gmail.com>
2026-02-15 17:08:44 +0300

    Rinat Hw

--- alterqueries.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE service.contracts
-DROP COLUMN headerText;
-
-ALTER TABLE service.advertisiments 
-RENAME TO advertisements;
-
-ALTER TABLE service.transport 
-RENAME COLUMN powerOf TO horsepower;
